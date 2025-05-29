07254a7dd6c114f6541cb11731452dff2157dec4
nathan-whitehead <nathanj.whitehead@icloud.com>
2025-05-29 12:01:26 -0500

    Improved content schema for better image handling

--- website/src/content/config.js
+++ website/src/content/config.js
@@ -3,27 +3,31 @@ import { defineCollection, z } from "astro:content";
 const blogCollection = defineCollection({
   type: "content",
   contentType: "mdx",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      date: z.date(),
-      category: z.string(),
-      description: z.string(),
-      tags: z.array(z.string()).optional(),
-      draft: z.boolean().optional().default(false),
-      thumbnail: z
-        .object({
-          src: image(),
-          alt: z.string().optional(),
-        })
-        .optional(),
-      cover: z
-        .object({
-          src: image(),
-          alt: z.string().optional(),
-        })
-        .optional(),
-    }),
+  schema: ({ image }) => {
+    const imageSchema = z.object({
+      src: image(),
+      alt: z.string().optional(),
+    });
+
+    return z
+      .object({
+        title: z.string(),
+        date: z.date(),
+        category: z.string(),
+        description: z.string(),
+        tags: z.array(z.string()).optional(),
+        draft: z.boolean().optional().default(false),
+        thumbnail: imageSchema.optional(),
+        cover: imageSchema.optional(),
+      })
+      .transform((data) => {
+        //if no thumbnail but has cover, set use thumbnail as cover
+        if (!data.thumbnail && data.cover) {
+          data.thumbnail = data.cover;
+        }
+        return data;
+      });
+  },
 });
 
 export const collections = {
